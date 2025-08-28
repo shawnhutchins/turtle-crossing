7bbc1f9877713d133d266a6bdf8414d77b6e6e5c
shawn <shawnhutchins1@gmail.com>
2025-08-27 21:22:51 -0400

    Created turtle screen and update loop

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+screen.setup(600, 600)
+screen.title("Turtle Crossing")
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+
+    screen.update()
+
+screen.exitonclick()
