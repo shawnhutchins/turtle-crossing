e9d5104a6dd8dbf1c1a8159e5d99527772a35989
shawn <shawnhutchins1@gmail.com>
2025-09-03 21:29:26 -0400

    Added generating cars every 6th loop

--- main.py
+++ main.py
@@ -10,17 +10,27 @@ screen.title("Turtle Crossing")
 screen.tracer(0)
 
 player = Player()
-car = Car(randint(-250, 250))
+cars = []
 
 screen.onkeypress(key="Up", fun=player.move)
 screen.listen()
 
+loopcount = 1
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
-    car.move()
-    if car.xcor() < -320:
-        car.restart()
+
+    # generate cars
+    if loopcount >= 6:
+        randycord = randint(-250, 250)
+        cars.append(Car(randycord))
+        loopcount = 0
+
+    # update cars
+    for car in cars:
+        car.move()
+
     screen.update()
+    loopcount += 1
 
 screen.exitonclick()
