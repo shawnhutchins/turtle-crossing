f75ee8a060149225ef156232ae6ae4ad47d487b8
shawn <shawnhutchins1@gmail.com>
2025-09-04 21:43:35 -0400

    Renamed yrand to randomy for readability

--- car.py
+++ car.py
@@ -2,14 +2,14 @@ from turtle import Turtle
 
 class Car(Turtle):
 
-    def __init__(self, yrand, rgbcolor):
+    def __init__(self, randomy, rgbcolor):
         super().__init__()
         self.penup()
         self.setheading(180)
         self.shape("square")
         self.color(rgbcolor)
         self.shapesize(1, 2)
-        self.goto(320, yrand)
+        self.goto(320, randomy)
 
     def move(self):
         self.forward(10)
